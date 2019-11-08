5a198168c7d45af37b5c4b2016225d82861fc714
Loïc SENCE <lsence@fidme.com>
2019-11-08 16:54:27 +0100

    pod dependency

--- Example/Pods/Target Support Files/Pods-PTCardTabBar_Example/Pods-PTCardTabBar_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PTCardTabBar_Example/Pods-PTCardTabBar_Example-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BadgeHub/BadgeHub.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PTCardTabBar/PTCardTabBar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BadgeHub/BadgeHub.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PTCardTabBar/PTCardTabBar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
